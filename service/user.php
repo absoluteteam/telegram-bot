c1d2915292cffff4dd65ad5340eaeab6215eaf0c
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2021-11-13 09:48:19 +0300

    new methods of User: sendNotification, setOrderStatus, assignToOrder, getActiveOrder, getOrderStatus

--- service/user.php
+++ service/user.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../bot/api.php';
 class UserNotFoundException extends Exception {}
 class User {
     private int $id;
@@ -33,5 +34,37 @@ class User {
 
     public function getAccessLevel(): int {
         return $this->access;
+    }
+
+    public function sendNotification(string $text): void {
+        API::sendMessage($this->id, $text, 1);
+    }
+
+    public function setOrderStatus(int $status): void {
+        global $sql;
+        if (is_null($this->active_order)) throw new Exception();
+        $sql->query("UPDATE users SET active_status = '$status' WHERE user_id = '$this->user_id'");
+    }
+
+    public function assignToOrder(?int $orderid): void {
+        global $sql;
+        if (!is_null($this->active_order)) throw new Exception();
+        if (is_null($orderid)) {
+            $sql->query("UPDATE users SET active_order = NULL, active_status = NULL WHERE user_id = '$this->user_id'");
+            $this->active_status = NULL;
+            $this->active_order = NULL;
+        } else {
+            $sql->query("UPDATE users SET active_order = {$orderid}, active_status = 0 WHERE user_id = '$this->user_id'") or die($sql->error);
+            $this->active_order = $orderid;
+            $this->active_status = 0;
+        }
+    }
+
+    public function getActiveOrder(): int|null {
+        return $this->active_order;
+    }
+
+    public function getOrderStatus(): int|null {
+        return $this->active_status;
     }
 }
