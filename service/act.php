8b9641246ce547530e4b10ffd538e6e829c729b9
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2021-11-14 04:15:57 +0300

    ActConstructor is now appending act number to db

--- service/act.php
+++ service/act.php
@@ -1,13 +1,17 @@
 require_once 'database.php';
+require_once 'order.php';
 class ActConstructor {
     function __construct(User $creator, string $img_path, float $lat, float $lon, int $type = 0) {
         global $sql;
         $stmt = $sql->prepare("INSERT INTO acts(creator_id, pic_path, type, geo_data_lat, geo_data_lon) VALUES (?, ?, ?, ?, ?)");
         $id = $creator->getUserID();
         $stmt->bind_param("isidd", $id, $img_path, $type, $lat, $lon);
-        if ($type == 0) {
-            $orderid = $creator->getActiveOrder();
-        }
         if ($stmt->execute() === false) throw new Exception($stmt->error);
+        $actid = $stmt->insert_id;
+        $stmt->close();
+        if ($type == 0) {
+            $order = $creator->getActiveOrder();
+            $order->appendAct($actid);
+        }
     }
 }
