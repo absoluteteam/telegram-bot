8ca375b4bcee3466d3df865de18c971e7c97a5b9
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2021-11-14 04:18:38 +0300

    Get all used files in requireall

--- service/requireall.php
+++ service/requireall.php
@@ -3,8 +3,9 @@
  * Fast require file
  */
 
-require 'database.php'; // Database connection
-require '../bot/api.php'; // API methods
-require 'user.php'; // User
-require 'act.php'; // Acts
-require 'order.php'; // Orders
+require_once 'database.php'; // Database connection
+require_once '../bot/api.php'; // API methods
+require_once 'user.php'; // User
+require_once 'act.php'; // Acts
+require_once 'order.php'; // Orders
+require_once 'company.php'; // Companies and shops
